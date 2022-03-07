11d2f575d4f02fab9e8e595c473d63780bb96388
Indie Blanc <indie.blanc@viacesi.fr>
2022-03-07 14:07:40 +0100

    feature: Grid initialization

--- main/grid.js
+++ main/grid.js
@@ -1,13 +1,18 @@
-function createGrid(randomSeed) {
+function createGrid(images) {
     const grid = []
     for (let i = 0; i < 5; i++) {
         const row = []
         for (let j = 0; j < 4; j++) {
-            row.push({visible:false,face:'back'})
+            let img = images[Math.floor(Math.random()*images.length)];
+            while(img.count >= 2){
+                img = images[Math.floor(Math.random()*images.length)];
+            }
+            row.push({visible:false,face:'back', value: img.src})
+            img.count = img.count + 1;
         }
-        grid.push(row)      
+        grid.push(row);      
     }
-    return grid
+    return grid;
 }
 
 export {createGrid}
